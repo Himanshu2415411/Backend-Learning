813603adf7be0974050850e071533074aad2b7aa
Himanshu2415411 <himanshu.wago@gmail.com>
2026-01-30 00:50:03 +0530

    mongoDB atlas connection completed

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";    
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this can be used alternatively to connect to the DB and start the server
+/*
+import express from "express"
+const app = express();
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (err) => {
+            console.log("Error connecting to the server", err);
+        })
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 3000}`);
+        });
+    }
+    catch(error){
+        console.log("Error connecting to MongoDB", error);
+    }
+    
+})()
+*/
