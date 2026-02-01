40b95756ad12afbe2a84eb7f9195765c0d12384d
Himanshu2415411 <himanshu.wago@gmail.com>
2026-02-01 19:26:29 +0530

    add user and video models; implement password hashing and JWT token generation

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String, // cloudinary public_id
+            required: true,
+        },
+        thumbnail:{
+            type: String, // cloudinary public_id
+            required: true,
+        },
+        title:{
+            type: String,
+            required: true,
+        },
+        description:{
+            type: String,
+        },
+        duration:{
+            type: Number, // in seconds
+            required: true,
+        },
+        views:{
+            type: Number,
+            default: 0,
+        },
+        isPublished:{
+            type: Boolean,
+            default: true,
+        },
+        owner:{
+            type : Schema.Types.ObjectId,
+            ref : "User",
+            required: true,
+        }
+
+
+
+    },
+    {timestamps: true}
+);
+
+export const Video = mongoose.model("Video", videoSchema);
